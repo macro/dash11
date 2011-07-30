1429bcab8f94b8fa673686df17ca881ceecd227d
Neil Chintomby <mace033@gmail.com>
2011-07-30 15:45:16 -0400

    updated prod config

--- dash_proj/settings_overrides/prod.py
+++ dash_proj/settings_overrides/prod.py
@@ -1,13 +1,13 @@
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dash',
         'USER': 'pyrun',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'PASSWORD': 'ajfhkdsrypqwe',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
