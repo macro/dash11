d57085c0041185648f001ee768cd26e75b13a5db
Neil Chintomby <mace033@gmail.com>
2011-07-30 13:19:28 -0700

    deploy via fabric

---
+++ dash_proj/fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import cd, run, sudo, env
+from fabric.contrib.files import exists
+
+
+env.user = 'pyrun'
+env.hosts = ['li329-155.members.linode.com']
+
+def hup():
+    sudo('svc -h /etc/service/gitawesome')
+
+def deploy(hard=False):
+    """
+    """
+    with cd('/home/pyrun/dash11/dash_proj/'):
+        if exists('.deploy.lck'):
+            print 'ABORT: Another upgrade is in progress, exiting...'
+            return
+        run('touch .deploy.lck')
+        run('git pull')
+        run('. ../virtualenv/bin/activate && \
+                pip install -r requirements_prod.txt && \
+                FLAVOR=prod python manage.py migrate')
+        hup()
+        run('rm .deploy.lck')
